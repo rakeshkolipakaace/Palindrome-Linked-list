3ff855e3188f7b37438128a7ec7e73ccebfe1f53
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-24 22:56:55 +0530

    solutions.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -21,6 +21,32 @@ struct ListNode* reverseList(struct ListNode* head) {
 
 // Check if list is palindrome
 bool isPalindrome(struct ListNode* head) {
-   
-    return true;//.......
+    if (!head || !head->next) return true;
+
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    if (fast) slow = slow->next;
+
+    struct ListNode* secondHalf = reverseList(slow);
+    struct ListNode* firstHalf = head;
+    struct ListNode* copy = secondHalf;
+
+    bool palindrome = true;
+    while (secondHalf) {
+        if (firstHalf->val != secondHalf->val) {
+            palindrome = false;
+            break;
+        }
+        firstHalf = firstHalf->next;
+        secondHalf = secondHalf->next;
+    }
+
+    reverseList(copy);
+    return palindrome;
 }
