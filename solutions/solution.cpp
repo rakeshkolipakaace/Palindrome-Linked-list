3bf147272be07299cf47567c33a5a5050876cc14
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-24 19:00:35 +0530

    solutions.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,16 +1,15 @@
 // solution.cpp
-#include <iostream>
-
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        
         
         return true;
     }
