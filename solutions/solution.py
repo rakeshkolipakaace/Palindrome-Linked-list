a341d547317eecc94cb9255e3b684887c137cd8d
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-24 20:14:06 +0530

    base.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -10,30 +10,4 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        if not head or not head.next:
-            return True
-
-        # Step 1: Find middle using slow/fast pointers
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        # Step 2: Reverse second half
-        prev = None
-        curr = slow
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-
-        # Step 3: Compare first half and reversed second half
-        left, right = head, prev
-        while right:
-            if left.val != right.val:
-                return False
-            left = left.next
-            right = right.next
-
-        return True
+        
