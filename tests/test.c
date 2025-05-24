3ea4a83e84c372866157565847a93e48d56cbf3d
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-24 23:33:54 +0530

    test..c

--- tests/test.c
+++ tests/test.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "../solutions/solution.c"  // Include the actual reverseList function
+
 
 // Declare ListNode and isPalindrome (defined in solution.c)
 struct ListNode {
