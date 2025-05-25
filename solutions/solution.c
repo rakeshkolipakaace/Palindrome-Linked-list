abda2f937457bca4ac73e9da902e0d5d5e78ce5e
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-25 10:20:14 +0530

    c sol

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,8 +1,7 @@
-// solution.c
 #include <stdbool.h>
 #include <stdlib.h>
 
-// Definition for singly-linked list....
+// Definition for singly-linked list
 struct ListNode {
     int val;
     struct ListNode *next;
@@ -23,32 +22,7 @@ struct ListNode* reverseList(struct ListNode* head) {
 
 // Check if list is palindrome
 bool isPalindrome(struct ListNode* head) {
-    if (!head || !head->next) return true;
+   
 
-    struct ListNode* slow = head;
-    struct ListNode* fast = head;
-
-    while (fast && fast->next) {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-
-    if (fast) slow = slow->next;
-
-    struct ListNode* secondHalf = reverseList(slow);
-    struct ListNode* firstHalf = head;
-    struct ListNode* copy = secondHalf;
-
-    bool palindrome = true;
-    while (secondHalf) {
-        if (firstHalf->val != secondHalf->val) {
-            palindrome = false;
-            break;
-        }
-        firstHalf = firstHalf->next;
-        secondHalf = secondHalf->next;
-    }
-
-    reverseList(copy);
-    return palindrome;
+    return true;
 }
