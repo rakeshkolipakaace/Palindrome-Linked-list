5e4448a590f398eb5e645b1f56ff2009c9ba871c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-25 12:07:16 +0530

    basec code

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,28 +1,12 @@
 #include <stdbool.h>
-#include <stdlib.h>
 
-// Definition for singly-linked list
 struct ListNode {
     int val;
     struct ListNode *next;
 };
 
-// Reverse a linked list
-struct ListNode* reverseList(struct ListNode* head) {
-    struct ListNode* prev = NULL;
-    struct ListNode* curr = head;
-    while (curr) {
-        struct ListNode* next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
-// Check if list is palindrome
 bool isPalindrome(struct ListNode* head) {
    
-// Write your code here...
- 
+    
+   
 }
